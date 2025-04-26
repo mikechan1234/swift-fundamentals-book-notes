a6c2be742a5165a6e1e1a2a09505a4a88d49f6c7
Michael Chan <mikechan1234@gmail.com>
2025-04-26 22:17:31 +0100

    Initial Commit

---
+++ swift-fundamentals-learnings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-fundamentals-learnings
+//
+//  Created by Michael Chan on 26/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-fundamentals-learnings/swift_fundamentals_learningsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_fundamentals_learningsApp.swift
+//  swift-fundamentals-learnings
+//
+//  Created by Michael Chan on 26/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct swift_fundamentals_learningsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
